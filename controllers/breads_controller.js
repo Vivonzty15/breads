4cfd38fa6be379ea6bb2aef3e0137e1657055bff
Braxton Allen <braxtonallen99@yahoo.com>
2022-02-28 20:57:33 -0600

    adding put method

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -19,6 +19,14 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
 // SHOW 
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -49,6 +57,17 @@ breads.post('/', (req, res) => {
 breads.delete('/:arrayIndex', (req, res) => {
     Bread.splice(req.params.arrayIndex, 1)
     res.status(303).redirect('/breads')
-  })
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body // not secure code, shorthand for now
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+})
 
 module.exports = breads
